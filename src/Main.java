0093a2634f1cd09f38d17020ead1d43af8013ea6
JulianAtencio <juat699@gmail.com>
2013-05-15 08:07:22 -0500

    un cambio en la clase main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] parametro) {
         System.out
-                .println("=============Aja Programa de MODIFICADO DOS prueba para GIT CON hub============");
+                .println("=============Aja Cambio de Julian Repo Programa de MODIFICADO DOS prueba para GIT CON hub============");
         ImprimeDos imprimeDos = new ImprimeDos();
         imprimeDos.devuelve_mensajeDos(true);
         // imprimeDos.devuelve_mensajeDos(false);
